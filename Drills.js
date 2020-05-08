a3a57d6eb3d8868cadb4c4ff70b341f0db8dcb8e
Javori Smart <jsmart1993@hotmail.com>
2020-05-08 12:40:32 -0400

    question 2

--- Drills.js
+++ Drills.js
@@ -1,7 +1,8 @@
 class _Node {
-  constructor(data, next) {
+  constructor(data, next, prev) {
     this.data = data;
     this.next = next;
+    this.prev = prev;
   }
 }
 
@@ -12,10 +13,10 @@ class Stack {
 
   push(data) {
     if(this.top === null) {
-      this.top === new _Node(data, null);
+      this.top = new _Node(data, null);
       return this.top
     }
-    const node = new _Node(data, this.top);
+    const node = new _Node(data, null, this.top);
     this.top = node;
   }
 
@@ -26,3 +27,47 @@ class Stack {
   }
 }
 
+function peek(stack) {
+  return stack.top.data;
+}
+
+function isEmpty(stack) {
+  return stack.top.data === null;
+}
+
+function display(stack) {
+  let currentStack = stack.top
+
+  while(currentStack) {
+    console.log(currentStack.data)
+    currentStack = currentStack.prev
+    
+  }
+  
+}
+
+function remove(item, stack) {
+  let currentStack = stack.top 
+  let previous = stack.top
+
+  while(currentStack.data !== item) {
+    currentStack = currentStack.prev
+    previous = currentStack.prev
+  }
+  previous.next = currentStack.next
+  console.log(previous)
+  console.log(currentStack)
+  currentStack.prev = previous;
+}
+
+function main() {
+  const starTrek = new Stack();
+    starTrek.push('Kirk');
+    starTrek.push('Spock');
+    starTrek.push('Mccoy');
+    starTrek.push('Scotty');
+    remove('Mccoy', starTrek);
+    display(starTrek);
+  }
+
+main();
